b86b8ee34c0c74d0ed2fe5e3e44b6cd62621051d
ncuxoxupypr <eu.kondratev@yandex.ru>
2021-06-05 22:40:57 +0300

    Trigger codecov

---
+++ src/test/java/TriggerTest.java
@@ -0,0 +1,9 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TriggerTest {
+    @Test
+    public void test() {
+        Assert.assertEquals(1, new Trigger().someLogic());
+    }
+}
